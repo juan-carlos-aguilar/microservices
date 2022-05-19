8f1c64c67be1eafdcabdb08221cf86f9e5a098cb
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-05-19 12:01:00 -0500

    Initial Commit

---
+++ service-product/src/main/java/academy/digitallab/store/product/entity/Product.java
@@ -0,0 +1,41 @@
+package academy.digitallab.store.product.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Field cannot be empty")
+    private String name;
+    private String description;
+    @Positive(message = "Stock should be greater than 0")
+    private Double stock;
+    private Double price;
+    private String status;
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @NotNull(message = "Field cannot be empty")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
