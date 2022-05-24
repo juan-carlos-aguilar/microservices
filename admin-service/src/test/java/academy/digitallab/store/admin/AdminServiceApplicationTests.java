d8d8486f8b53218710130c9e9156341b97fac6df
Juan Aguilar <juan-carlos.aguilar@atos.net>
2022-05-24 09:53:02 -0500

    Added admin-service project

---
+++ admin-service/src/test/java/academy/digitallab/store/admin/AdminServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package academy.digitallab.store.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@EnableAdminServer
+@SpringBootApplication
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
